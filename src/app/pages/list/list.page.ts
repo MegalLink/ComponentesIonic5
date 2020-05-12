eb9f632b2a67df97c32c272e04b2035d90ae3892
MegalLink <50426219+MegalLink@users.noreply.github.com>
2020-05-12 16:10:03 -0500

    Implementacion Toast

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -10,15 +10,30 @@ import { IonList } from '@ionic/angular';
 export class ListPage implements OnInit {
   @ViewChild('lista', {static:false}) lista:IonList;
 users:any;
-  constructor(private dataS:DataService) { }
+  constructor(private dataS:DataService,private toastC:ToastController) { }
 
   ngOnInit() {
     this.dataS.getUsers().subscribe(resp=>{this.users=resp})
   }
  favorito(user){
+  this.presentToast("Agregado a favoritos")
 this.lista.closeSlidingItems();
+
 }
  borrar(user){
+  this.presentToast("Presiono Borrar")
   this.lista.closeSlidingItems();
 }
+
+
+
+async presentToast(message:string) {
+  const toast = await this.toastC.create({
+    message: message ,
+    duration: 2000
+  });
+  toast.present();
+}
+
+
 }
